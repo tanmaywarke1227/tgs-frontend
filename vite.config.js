9d0f3b76ba5c33bc004db37729a6324da169b0fd
tanmaywarke1227 <tanmaywarke1227@gmail.com>
2026-04-28 23:08:23 +0530

    url

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://tgs-backend-ifpp.onrender.com',
         changeOrigin: true,
       }
     }
